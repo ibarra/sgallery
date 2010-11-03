7ed3ddf3bcf56285e06c000e8b8261fa65095b1b
Martin Ibarra <mic@misretratos.com>
2010-11-03 15:37:16 -0700

    add sfGravatar and sfThumbnails

--- apps/backend/modules/imagesgallery/templates/indexSuccess.php
+++ apps/backend/modules/imagesgallery/templates/indexSuccess.php
@@ -1,3 +1,9 @@
+<?php
+	use_helper("jQuery");
+	use_helper("sfJQueryLightbox");
+ ?>
+
+
 <h1>Sf imagess List</h1>
 
 <table>
@@ -17,7 +23,14 @@
     <?php foreach ($sf_imagess as $sf_images): ?>
     <tr>
       <td><a href="<?php echo url_for('imagesgallery/show?id_image='.$sf_images->getIdImage()) ?>"><?php echo $sf_images->getIdImage() ?></a></td>
-      <td><?php echo $sf_images->getGalleryId() ?></td>
+      <td>
+         <?php echo light_image("/uploads/gallery/s_".
+      		$sf_images->getGalleryId(), "/uploads/gallery/".$sf_images->getGalleryId(), $image_link_options
+      		= array('title' => $sf_images->getDescription() . ' ' . $sf_images->getCreatedAt() ));
+      ?>
+
+
+          <img src="/uploads/gallery/s_<?php echo $sf_images->getGalleryId()?>" /></td>
       <td><?php echo $sf_images->getUserId() ?></td>
       <td><?php echo $sf_images->getDescription() ?></td>
       <td><?php echo $sf_images->getOrder() ?></td>
@@ -29,4 +42,4 @@
   </tbody>
 </table>
 
-  <a href="<?php echo url_for('imagesgallery/new') ?>">New</a>
+  
